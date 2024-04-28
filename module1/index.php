8904e7768345478128ec944c0fe3e57661ffc6e9
Alex Madrazo <alex.madrazo@bgamestudios.com>
2024-04-28 13:27:12 +0200

    adding readme

---
+++ module1/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Welcome to PHP Learning Series - Module 1</h1>";
+echo "<p>This is your first PHP script running inside a Docker container!</p>";
+phpinfo();  // This function outputs information about PHP's configuration
+?>
