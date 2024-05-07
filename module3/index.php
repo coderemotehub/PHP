d4179571c47f3673735cea1005f4a41c7d21fb18
Alex Madrazo <alex.madrazo@bgamestudios.com>
2024-05-07 11:35:56 +0200

    adding ficheros

--- module3/index.php
+++ module3/index.php
@@ -4,27 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <h1>Formularios</h1>
     <form action="submit.php" method="POST">
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" id="nombre">
-        <br>
-        <label for="apellidos">Apellidos:</label>
-        <input type="text" name="apellidos" id="apellidos">
-        <br>
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email">
-        <br>
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" id="password">
-        <br>
-        <label for="edad">Edad:</label>
-        <input type="number" name="edad" id="edad">
-        <br>        
-        <label for="boton">Botón:</label>
-        <input type="submit" name="boton" id="boton" value="Enviar">
+        <div class="m-2">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" id="nombre">
+        </div>
+        <div class="m-2">
+            <label for="apellidos">Apellidos:</label>
+            <input type="text" name="apellidos" id="apellidos">
+        </div>
+        <div class="m-2">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <div class="m-2">
+            <label for="password">Contraseña:</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div class="m-2">
+            <label for="edad">Edad:</label>
+            <input type="number" name="edad" id="edad">
+        </div>
+        <div class="m-4">
+            <label for="boton">Botón:</label>
+            <input type="submit" name="boton" id="boton" value="Enviar">
+        </div>      
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
